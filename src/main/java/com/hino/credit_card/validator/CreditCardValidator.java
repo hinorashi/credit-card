5725e766de1bbd3443161df2f163c35f6669fd77
hino_hatake <sinhngay3110@gmail.com>
2019-06-15 15:35:14 +0700

    oh shit null pointer exception

--- src/main/java/com/hino/credit_card/validator/CreditCardValidator.java
+++ src/main/java/com/hino/credit_card/validator/CreditCardValidator.java
@@ -13,6 +13,10 @@ public class CreditCardValidator implements ConstraintValidator<CreditCardValid,
 
   @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
+
+    if (value == null)
+      return false;
+
     return pattern.matcher(value).matches();
   }
 
